a033d6b1a5d546f986824a7591f07e45e6ba4bd3
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-11-24 16:37:02 +0530

    Initial project setup

---
+++ backend/cropsystem/src/main/java/lk/ijse/spring/cropSystem/CropsystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.cropSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropsystemApplication.class, args);
+	}
+
+}
---
+++ backend/cropsystem/src/test/java/lk/ijse/spring/cropSystem/CropsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.cropSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
